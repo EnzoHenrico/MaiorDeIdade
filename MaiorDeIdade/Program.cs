060445e053aeff12546026630d5122744d1d83fb
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-19 00:35:52 -0300

    Add project files.

---
+++ MaiorDeIdade/Program.cs
@@ -0,0 +1,20 @@
+﻿int age;
+
+Console.Write("Digite sua idade: ");
+age = int.Parse(Console.ReadLine());
+
+if (age < 0)
+{
+    Console.WriteLine("Idade inválida.");
+}
+else
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Você já é maior de idade!");
+    }
+    else
+    {
+        Console.WriteLine("Você ainda é menor de idade.");
+    }
+}
